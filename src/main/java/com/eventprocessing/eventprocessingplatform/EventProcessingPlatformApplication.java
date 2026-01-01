d9eefe4444471bc7271f83e39bf8232fb6d0c79c
nimesh.viduranga <nimesha.v@nic.lk>
2026-01-01 16:16:43 +0530

    inital configs

---
+++ src/main/java/com/eventprocessing/eventprocessingplatform/EventProcessingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.eventprocessing.eventprocessingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventProcessingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventProcessingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventprocessing/eventprocessingplatform/EventProcessingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventprocessing.eventprocessingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventProcessingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
